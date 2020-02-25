aa453276cab39097c2387f59bfb3f992f6fa82e6
DrGooseman <jamesjquinn12@gmail.com>
2020-02-25 21:16:28 +0300

    adds create-user component

--- src/components/create-user.component.jsx
+++ src/components/create-user.component.jsx
@@ -1,5 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function CreateUser() {
-  return <h1>CreateUser</h1>;
+  const [username, setUsername] = useState("");
+
+  function onChangeUsername(e) {
+    setUsername(e.target.value);
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+
+    const user = {
+      username
+    };
+
+    console.log(user);
+    setUsername("");
+  }
+
+  return (
+    <div>
+      <h3>Create New User</h3>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>Username: </label>
+          <input
+            type="text"
+            required
+            className="form-control"
+            value={username}
+            onChange={onChangeUsername}
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="submit"
+            value="Create User"
+            className="btn btn-primary"
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
